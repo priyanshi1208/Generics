db3642f8a9efba0d431c9f97f96c9a60cf76fa8f
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-03 15:41:40 +0530

    ADDED:Method to fing maximum of float datatypes

--- src/main/java/com/magic/generics/FindMaximum.java
+++ src/main/java/com/magic/generics/FindMaximum.java
@@ -4,4 +4,7 @@ public class FindMaximum {
 public static Integer MaximumNumber(int x, int y, int z){
     return (z>y)?(z>x?z:x):(y>x?y:x);
 }
+    public static Float MaximumNumber(float x, float y, float z){
+        return (z>y)?(z>x?z:x):(y>x?y:x);
+    }
 }
--- src/test/java/com/magic/generics/FindMaximumTest.java
+++ src/test/java/com/magic/generics/FindMaximumTest.java
@@ -23,4 +23,24 @@ class FindMaximumTest {
         int result = FindMaximum.MaximumNumber(7, 9, 17).compareTo(9);
         Assert.assertEquals(1,result);
     }
+    @Test
+    void maximum_NumberFloat_At_Position_1() {
+        int result = FindMaximum.MaximumNumber(7.4f, 5.3f, 2.8f).compareTo(7.4f);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    void maximum_NumberFloat_At_Position_2() {
+        int result = FindMaximum.MaximumNumber(7.5f, 9.7f, 2.8f).compareTo(9.7f);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    void maximum_NumberFloat_At_Position_3() {
+        int result = FindMaximum.MaximumNumber(7.9f, 9.5f, 17.9f).compareTo(17.9f);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    void maximum_NumberFloat_At_Position_3_Is_False() {
+        int result = FindMaximum.MaximumNumber(7.2f, 9.3f, 17.5f).compareTo(9.3f);
+        Assert.assertEquals(1,result);
+    }
 }
