8ba7f766fb27405bfa81b8915600e36ec9b3eba2
priyanshi1208 <pri.shukla2208@gmail.com>
2021-06-03 14:27:24 +0530

    ADDED:test cases to check maximum at different positions

--- src/test/java/com/magic/generics/FindMaximumTest.java
+++ src/test/java/com/magic/generics/FindMaximumTest.java
@@ -8,4 +8,19 @@ class FindMaximumTest {
         int result = FindMaximum.MaximumNumber(7, 5, 2).compareTo(7);
         Assert.assertEquals(0,result);
     }
+    @Test
+    void maximum_Number_At_Position_2() {
+        int result = FindMaximum.MaximumNumber(7, 9, 2).compareTo(9);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    void maximum_Number_At_Position_3() {
+        int result = FindMaximum.MaximumNumber(7, 9, 17).compareTo(17);
+        Assert.assertEquals(0,result);
+    }
+    @Test
+    void maximum_Number_At_Position_3_Is_False() {
+        int result = FindMaximum.MaximumNumber(7, 9, 17).compareTo(9);
+        Assert.assertEquals(1,result);
+    }
 }
